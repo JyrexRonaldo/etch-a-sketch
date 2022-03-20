fec8c8fc5f71a1ede79bd8e83d88c2defe84e477
Jyrex Ronaldo <jyrexgt38@gmail.com>
2022-03-20 20:41:35 +0000

    Randomize colors being displayed

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ for (let i = 0; i <= 10000; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
     div.addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'red';
+        e.target.style.backgroundColor = `hsl(${randomHue()}, 100%, 50%)`;
     });
 
     button.addEventListener('click', () => {
@@ -26,4 +26,9 @@ for (let i = 0; i <= 10000; i++) {
         div.style.width = `${600 / size}px`;
     });
 
+}
+
+function randomHue() {
+    let deg = Math.floor(Math.random() * 360);
+    return deg;
 }
