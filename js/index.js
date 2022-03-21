fdf749ea4d32b1fbd616fbf715d171ea34bfcc40
Jyrex Ronaldo <jyrexgt38@gmail.com>
2022-03-21 11:20:13 +0000

    Remove console statements and introduce descriptive variable

--- js/index.js
+++ js/index.js
@@ -14,13 +14,12 @@ for (let i = 0; i <= 10000; i++) {
     const div = document.createElement('div');
     container.appendChild(div);
     let count = 0;
-    let n = 100;
+    let lightnessVal = 100;
     div.addEventListener('mouseover', (e) => {
         if (e.target.style.backgroundColor = true) {
             while (count <= 9) {
-                n = n - 10;
-                console.log(n);
-                e.target.style.backgroundColor = `hsl(${randomHue()}, 100%, ${n}%)`;
+                lightnessVal = lightnessVal - 10;
+                e.target.style.backgroundColor = `hsl(${randomHue()}, 100%, ${lightnessVal}%)`;
                 break;        
             }
             count++;   
@@ -35,7 +34,7 @@ for (let i = 0; i <= 10000; i++) {
         div.style.height = `${600 / size}px`;
         div.style.width = `${600 / size}px`;
         count = 0;
-        n = 100;
+        lightnessVal = 100;
     });
 
 }
